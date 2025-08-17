6d1898c171da95d1c21de2c15ce821fe37753b00
elithsar <joffrey.boucy@gmail.com>
2025-08-17 22:13:29 +0200

    initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/elithsar/go-http-server
+
+go 1.24.5
---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import(
+    "fmt"
+    "net/http"
+)
+
+func main(){
+    serveMux := http.NewServeMux()
+    server := http.Server{
+	Addr: ":8080",
+	Handler: serveMux,
+    }
+    err := http.ListenAndServe(server.Addr, server.Handler)
+
+    if err != nil {
+	fmt.Errorf("failed to init server: %w", err)
+    }
+}
