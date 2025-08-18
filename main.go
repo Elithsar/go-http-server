dcf4373e3aa1337a327e374f52446e36abe0ef9c
elithsar <joffrey.boucy@gmail.com>
2025-08-18 19:28:36 +0200

    Added assets

--- main.go
+++ main.go
@@ -11,9 +11,10 @@ func main(){
 	Addr: ":8080",
 	Handler: serveMux,
     }
+    serveMux.Handle("/", http.FileServer(http.Dir(".")))
+
     err := http.ListenAndServe(server.Addr, server.Handler)
 
-    if err != nil {
-	fmt.Errorf("failed to init server: %w", err)
+    if err != nil {                                   fmt.Errorf("failed to init server: %w", err)
     }
 }
