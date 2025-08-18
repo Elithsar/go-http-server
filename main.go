224c60c45d737114f24f6389f97fb67cbbdb285d
elithsar <joffrey.boucy@gmail.com>
2025-08-19 00:08:47 +0200

    Added custom route

--- main.go
+++ main.go
@@ -5,13 +5,20 @@ import(
     "net/http"
 )
 
+func readinessHandler(res http.ResponseWriter, req *http.Request) {
+    res.Header().Set("Content-Type", "text/plain; charset=utf-8")
+    res.WriteHeader(200)
+    res.Write([]byte("OK"))
+}
+
 func main(){
     serveMux := http.NewServeMux()
     server := http.Server{
 	Addr: ":8080",
 	Handler: serveMux,
     }
-    serveMux.Handle("/", http.FileServer(http.Dir(".")))
+    serveMux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))	
+    serveMux.HandleFunc("/healthz", readinessHandler)
 
     err := http.ListenAndServe(server.Addr, server.Handler)
 
