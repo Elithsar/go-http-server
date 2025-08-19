65e707f6562208f7a7a166e6f1160bb065d2cf71
elithsar <joffrey.boucy@gmail.com>
2025-08-19 22:29:57 +0200

    Added chirps post

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"sync/atomic"
@@ -40,6 +41,50 @@ func readinessHandler(res http.ResponseWriter, req *http.Request) {
 	res.Write([]byte("OK"))
 }
 
+func chirpIn(w http.ResponseWriter, r *http.Request) {
+	type request struct {
+		Body string `json:"body"`
+	}
+	type response struct {
+		Valid bool   `json:"valid,omitempty"`
+		Error string `json:"error,omitempty"`
+	}
+	w.Header().Set("Content-Type", "application/json")
+	res := response{}
+
+	decoder := json.NewDecoder(r.Body)
+	var message request
+	err := decoder.Decode(&message)
+
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		res.Error = "Something went wrong"
+		out, _ := json.Marshal(res)
+		w.Write(out)
+		return
+	}
+
+	MAX_SIZE := 140
+	if len(message.Body) > MAX_SIZE {
+		w.WriteHeader(http.StatusBadRequest)
+		res.Error = "Chirp is too long"
+		out, _ := json.Marshal(res)
+		w.Write(out)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+
+	res.Valid = true
+	jsonResp, err := json.Marshal(res)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("Something went wrong"))
+		return
+	}
+	w.Write(jsonResp)
+}
+
 func main() {
 	serveMux := http.NewServeMux()
 	cfg := apiConfig{}
@@ -50,6 +95,8 @@ func main() {
 	handler := http.StripPrefix("/app", http.FileServer(http.Dir(".")))
 	serveMux.Handle("/app/", cfg.middlewareMetricsInc(handler))
 	serveMux.HandleFunc("GET /api/healthz", readinessHandler)
+	serveMux.HandleFunc("POST /api/validate_chirp", chirpIn)
+
 	serveMux.HandleFunc("GET /admin/metrics", cfg.serverHitsHandler)
 	serveMux.HandleFunc("POST /admin/reset", cfg.serverHitsResetHandler)
 
